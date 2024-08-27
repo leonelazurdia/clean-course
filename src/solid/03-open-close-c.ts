0b04c21e8e3b3d57e05dc66f9361183f7031de9f
unknown <cazurdia@GTCA-17-147.grupozetagas.corp>
2024-08-27 15:50:42 -0600

    cambio de axiosa fetch, donde se demuestra que no se modificaron las otras classes

--- src/solid/03-open-close-c.ts
+++ src/solid/03-open-close-c.ts
@@ -1,12 +1,22 @@
-import axios from 'axios';
+//import axios from 'axios';
 
+//esta es una CLASE ADAPTADORA, es buena practica cuando uso clases de terceros.
 
 export class HttpClient {
 
      
-    async get(url: string){
+    /* async get(url: string){
         const{data, status} = await axios.get(url)
         return{data,status};
+    } */
+
+    async get(url:string){
+
+        //fetch sirve para hacer peticiones http
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        return {data: data,stauts: resp.status}
     }
     
 
