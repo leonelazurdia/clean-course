c8d56187ef90167982ce85800acb3dd311b17308
unknown <cazurdia@GTCA-17-147.grupozetagas.corp>
2024-08-30 14:21:51 -0600

    las clases no deben ser obligadas a usar metodos que no utilizan (aplicado)

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -1,47 +1,43 @@
 interface Bird{
+    eat(): void;  
+}
+interface FlyingBird{
     fly(): void;
-    eat(): void;
+}
+interface RuningBird{
+    run(): void;
+}
+interface SwimmingBird{
     run(): void;
 }
 
-class Tucan implements Bird{
 
-    public fly(){
-
-    }
-
-    public eat(){
-
-    }
-    public run(){
-
-    }
+class Tucan implements Bird, FlyingBird{
+    public fly(){}
+    public eat(){}
+   
 }
 
-class Humminbird implements Bird{
-    public fly(){
-
-    }
-
-    public eat(){
-
-    }
-
-    public run(){
-
-    }
-
+class Humminbird implements Bird, FlyingBird{
+    public fly(){}
+    public eat(){}
 }
 
-class Ostrich implements Bird{
-    fly(): void {
-        throw new Error("Esta ave no buela.");// el avestruz no vuela aqui se violenta el principio de segregación de interfaces.
-    }
+class Ostrich implements Bird, RuningBird{
+    
     eat(): void {
         throw new Error("Method not implemented.");
     }
     run(): void {
         throw new Error("Method not implemented.");
     }
+}
 
+class Pinguin implements Bird, SwimmingBird{
+    run(): void {
+        throw new Error("Method not implemented.");
+    }   
+    eat(): void {
+        throw new Error("Method not implemented.");
+    }
 }
